fb23baa793cf8f0a3339c5a20ea746c68a8156a2
André Luferat <33375030+Luferat@users.noreply.github.com>
2021-10-23 13:57:30 -0300

    AJustes

--- Aula_01/exercicio_07.py
+++ Aula_01/exercicio_07.py
@@ -6,4 +6,4 @@ A fórmula de conversão é: C = 5.0 * (F - 32.0)/9, sendo C a temperatura em Ce
 
 tempF = float(input('Digite a temperatura em Fahrenheit: ').replace(',', '.'))
 tempC = 5 * (tempF - 32)/9
-print(f'{tempF}ºF = {tempC}ºC')
+print(f'{tempF}°F = {tempC}°C')
